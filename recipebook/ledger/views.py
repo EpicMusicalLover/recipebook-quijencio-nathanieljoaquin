fc1a131f3406663e069251bed7424509210a7c37
Nathaniel Joaquin Quijencio <natesjoaquin@gmail.com>
2026-02-09 13:36:19 +0800

    removed the link for both recipe paths

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -5,32 +5,137 @@ def index(request):
     return HttpResponse('Recipe Book')
 
 def recipe_list(request):
-    if request.method =='POST':
-        tasks.append(request.POST.get('task_name'))
-    ctx={"tasks":tasks}
-    return render(request, 'recipe_list.html', ctx)
-tasks = ['Recipe 1', 'Recipe 2']
+    recipe = {
+        "recipes": [
+            {
+                "name": "Recipe 1",
+                "ingredients": [
+                    {
+                        "name": "tomato",
+                        "quantity": "3pcs"
+                    },
+                    {
+                        "name": "onion",
+                        "quantity": "1pc"
+                    },
+                    {
+                        "name": "pork",
+                        "quantity": "1kg"
+                    },
+                    {
+                        "name": "water",
+                        "quantity": "1L"
+                    },
+                    {
+                        "name": "sinigang mix",
+                        "quantity": "1 packet"
+                    }
+                ],
+                "link": "/recipe/1"
+            },
+            {
+                "name": "Recipe 2",
+                "ingredients": [
+                    {
+                        "name": "garlic",
+                        "quantity": "1 head"
+                    },
+                    {
+                        "name": "onion",
+                        "quantity": "1pc"
+                    },
+                    {
+                        "name": "vinegar",
+                        "quantity": "1/2cup"
+                    },
+                    {
+                        "name": "water",
+                        "quanity": "1 cup"
+                    },
+                    {
+                        "name": "salt",
+                        "quantity": "1 tablespoon"
+                    },
+                    {
+                        "name": "whole black peppers",
+                        "quantity": "1 tablespoon"
+                    },
+                    {
+                        "name": "pork",
+                        "quantity": "1 kilo"
+                    }
+                ],
+                "link": "/recipe/2"
+            }
+        ]
+    }
+    ctx={"recipe":recipe}
+    return render(request, 'recipe_details.html', ctx)
 
 def recipe_one(request):
     recipe = {
         "name": "Recipe 1",
         "ingredients": [
-            {"name": "tomato", "quantity": "3pcs"},
-            {"name": "onion", "quantity": "1pc"},
-            {"name": "pork", "quantity": "1kg"},
-            {"name": "water", "quantity": "1L"},
-            {"name": "sinigang mix", "quantity": "1 packet"}
-        ],
-        "link": "/ledger/recipe/1"
+            {
+                "name": "tomato",
+                "quantity": "3pcs"
+            },
+            {
+                "name": "onion",
+                "quantity": "1pc"
+            },
+            {
+                "name": "pork",
+                "quantity": "1kg"
+            },
+            {
+                "name": "water",
+                "quantity": "1L"
+            },
+            {
+                "name": "sinigang mix",
+                "quantity": "1 packet"
+            }
+        ]
     }
     ctx={"recipe":recipe}
-    return render(request, 'recipe_one.html', ctx)
+    return render(request, 'recipe_details.html', ctx)
 
 def recipe_two(request):
-    if request.method =='POST':
-        tasks.append(request.POST.get('task_name'))
-    ctx={"tasks":tasks}
-    return render(request, 'recipe_list.html', ctx)
-tasks = ['Recipe 1', 'Recipe 2']
+    recipe = {
+        "name": "Recipe 2",
+        "ingredients": [
+            {
+                "name": "garlic",
+                "quantity": "1 head"
+            },
+            {
+                "name": "onion",
+                "quantity": "1pc"
+            },
+            {
+                "name": "vinegar",
+                "quantity": "1/2cup"
+            },
+            {
+                "name": "water",
+                "quantity": "1 cup"
+            },
+            {
+                "name": "salt",
+                "quantity": "1 tablespoon"
+            },
+            {
+                "name": "whole black peppers",
+                "quantity": "1 tablespoon"
+            },
+            {
+                "name": "pork",
+                "quantity": "1 kilo"
+            }
+        ],
+    }
+    ctx={"recipe":recipe}
+    return render(request, 'recipe_details.html', ctx)
 
 # Create your views here.
