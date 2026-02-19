75f15da979fe649c1a95bf3a9d1c03a3f1ade22e
Nathaniel Joaquin Quijencio <natesjoaquin@gmail.com>
2026-02-19 08:46:05 +0800

    added class Ingredient, class Recipe, and class RecipeIngredient in models.py with their corresponding variables

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,6 +1,31 @@
 from django.db import models
 from django.urls import reverse
 
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
+
+class RecipeIngredient(models.Model):
+    quantity = models.CharField(max_length=100)
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE,
+        related_name='recipe_ingredients'
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='recipe_ingredients'
+    )
+    def __str__(self):
+        return f"{self.quantity} of {self.ingredient.name} in {self.recipe.name}"
+
 class TaskGroup(models.Model):
     name = models.CharField(max_length=50)
 
