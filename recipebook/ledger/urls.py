75205aefdb1360d236cd638bac988e0a762c1f44
Nathaniel Joaquin Quijencio <natesjoaquin@gmail.com>
2026-02-19 08:29:30 +0800

    changed the starting path in urls.py in recipebook

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from .views import recipe_list, recipe_one, recipe_two
+app_name = 'ledger'
 urlpatterns=[
     path('recipes/list', recipe_list,name="recipe-list"),
     path('recipe/1', recipe_one,name="recipe-one"),
     path('recipe/2', recipe_two,name="recipe-two"),
 ]
-app_name = 'ledger'
