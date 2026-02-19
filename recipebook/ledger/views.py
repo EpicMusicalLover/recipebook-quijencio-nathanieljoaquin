865c3c8107e847ff9421ed9a9744a8edcba964aa
Nathaniel Joaquin Quijencio <natesjoaquin@gmail.com>
2026-02-19 08:51:33 +0800

    removed the unnecessary index method in views.py

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse('string')
-
-
 def recipe_list(request):
     ctx={
         "recipes": [
