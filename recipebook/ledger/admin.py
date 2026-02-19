24212983dc6c31299856c9802f115c37ae021631
Nathaniel Joaquin Quijencio <natesjoaquin@gmail.com>
2026-02-19 13:49:41 +0800

    removed the class Task and class TaskGroup in models.py

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,25 +1,25 @@
 from django.contrib import admin
-from .models import Task, TaskGroup
+# from .models import Task, TaskGroup
 
-class TaskInLine(admin.TabularInline):
-    model = Task
+# class TaskInLine(admin.TabularInline):
+#     model = Task
 
-class TaskGroupAdmin(admin.ModelAdmin):
-    model = Task
-    inlines = [TaskInLine,]
-class TaskAdmin(admin.ModelAdmin):
-    model = Task
-    search_fields = ('name',)
-    list_display =('name','due_date',)
-    list_filter=('due_date',)
-    fieldsets =[
-        ('Details',{
-            'fields':[
-                ('name','taskgroup'),'due_date'
-            ]
-        })
-    ]
+# class TaskGroupAdmin(admin.ModelAdmin):
+#     model = Task
+#     inlines = [TaskInLine,]
+# class TaskAdmin(admin.ModelAdmin):
+#     model = Task
+#     search_fields = ('name',)
+#     list_display =('name','due_date',)
+#     list_filter=('due_date',)
+#     fieldsets =[
+#         ('Details',{
+#             'fields':[
+#                 ('name','taskgroup'),'due_date'
+#             ]
+#         })
+#     ]
 
-admin.site.register(Task, TaskAdmin)
-admin.site.register(TaskGroup, TaskGroupAdmin)
+# admin.site.register(Task, TaskAdmin)
+# admin.site.register(TaskGroup, TaskGroupAdmin)
 # Register your models here.
--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -29,26 +29,3 @@ class RecipeIngredient(models.Model):
     )
     def __str__(self):
         return f"{self.quantity} {self.ingredient.name}"
-
-class TaskGroup(models.Model):
-    name = models.CharField(max_length=50)
-
-class Task(models.Model):
-    name = models.CharField(max_length=100)
-    due_date = models.DateTimeField(null=False)
-    taskgroup = models.ForeignKey(
-        TaskGroup, 
-        on_delete=models.CASCADE, #if the group is erased, the task is also erased
-        related_name='tasks' #refer to all the tasks using related name in the future for easier search
-    )
-
-    def __str__(self):
-        return f"{self.name} due on {self.due_date}"
-    
-    def get_absolute_url(self): #will return the url
-        return reverse('tasks:task_detail',args=[str(self.id)])
-    
-    class Meta:
-        unique_together = ['due_date','name'] #dont create a duplicate task, another task with same due date and name
-        verbose_name = 'task'
-        verbose_name_plural = 'tasks'
