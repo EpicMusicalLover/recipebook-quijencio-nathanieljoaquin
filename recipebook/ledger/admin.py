98ea1a6a54e3d81b54f2454cd5751ab2ad308239
Nathaniel Joaquin Quijencio <natesjoaquin@gmail.com>
2026-02-19 13:54:48 +0800

    Added the site register function Recipe to admin.py

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,25 +1,4 @@
 from django.contrib import admin
-# from .models import Task, TaskGroup
+from .models import Recipe
 
-# class TaskInLine(admin.TabularInline):
-#     model = Task
-
-# class TaskGroupAdmin(admin.ModelAdmin):
-#     model = Task
-#     inlines = [TaskInLine,]
-# class TaskAdmin(admin.ModelAdmin):
-#     model = Task
-#     search_fields = ('name',)
-#     list_display =('name','due_date',)
-#     list_filter=('due_date',)
-#     fieldsets =[
-#         ('Details',{
-#             'fields':[
-#                 ('name','taskgroup'),'due_date'
-#             ]
-#         })
-#     ]
-
-# admin.site.register(Task, TaskAdmin)
-# admin.site.register(TaskGroup, TaskGroupAdmin)
-# Register your models here.
+admin.site.register(Recipe)
