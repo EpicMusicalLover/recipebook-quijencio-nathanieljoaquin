7ab3a37066132aa4b6597dfbec020ecf21fd998c
Nathaniel Joaquin Quijencio <natesjoaquin@gmail.com>
2026-02-19 13:53:43 +0800

    removed unnecessary module imports in views.py

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.generic.base import TemplateView
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from .models import Recipe
