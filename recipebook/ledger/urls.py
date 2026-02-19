1328dcff487678672caddcfb8e62ef3b50abf8f0
Nathaniel Joaquin Quijencio <natesjoaquin@gmail.com>
2026-02-19 09:46:38 +0800

    changed the paths in urls.py

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -4,6 +4,7 @@ app_name = 'ledger'
 urlpatterns=[
     #path('recipes/list', recipe_list,name="recipe-list"),
     path('recipes/list', RecipeListView.as_view(), name="task_list"),
-    path('recipe/1', recipe_one,name="recipe-one"),
-    path('recipe/2', recipe_two,name="recipe-two"),
+    path('recipe/<int:pk>', RecipeDetailView.as_view(), name="task_detail"),
+    # path('recipe/1', recipe_one,name="recipe-one"),
+    # path('recipe/2', recipe_two,name="recipe-two"),
 ]
