4c32be591543a4d6e233db3c430a3b3a7448f008
Nathaniel Joaquin Quijencio <natesjoaquin@gmail.com>
2026-02-19 13:46:52 +0800

    cleaned up the urls.py

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,11 +1,7 @@
 from django.urls import path
-from .views import recipe_list, recipe_one, recipe_two, RecipeListView, RecipeDetailView
+from .views import RecipeListView, RecipeDetailView
 app_name = 'ledger'
 urlpatterns=[
-    #path('recipes/list', recipe_list,name="recipe-list"),
     path('recipes/list/', RecipeListView.as_view(), name="recipe_list"),
     path('recipe/<int:pk>/', RecipeDetailView.as_view(), name="recipe_detail"),
-    # path('recipe/1', recipe_one,name="recipe-one"),
-    # path('recipe/2', recipe_two,name="recipe-two"),
-    # fix the paths
 ]
