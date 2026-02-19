97f0520d981366e4aa5200ad12789e02e716dd08
Nathaniel Joaquin Quijencio <natesjoaquin@gmail.com>
2026-02-19 08:49:33 +0800

    added the get_absolute_url for both the Ingredient and Recipe class in models.py

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -5,11 +5,15 @@ class Ingredient(models.Model):
     name = models.CharField(max_length=255)
     def __str__(self):
         return self.name
+    def get_absolute_url(self):
+        return reverse('ingredient_detail', args=[str(self.id)])
 
 class Recipe(models.Model):
     name = models.CharField(max_length=255)
     def __str__(self):
         return self.name
+    def get_absolute_url(self):
+        return reverse('recipe_detail', args=[str(self.id)])
 
 class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=100)
