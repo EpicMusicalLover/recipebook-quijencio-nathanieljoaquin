8fdb53f31624050c03957e09f6e0d53bca22cbb4
Nathaniel Joaquin Quijencio <natesjoaquin@gmail.com>
2026-02-19 09:42:12 +0800

    added recipelistview an drecipedetail view methods in views.py and added the corresponding urls to urls.py

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import recipe_list, recipe_one, recipe_two
+from .views import recipe_list, recipe_one, recipe_two, RecipeListView, RecipeDetailView
 app_name = 'ledger'
 urlpatterns=[
     #path('recipes/list', recipe_list,name="recipe-list"),
-    path('recipes/list', recipe_list, name="task_list"),
+    path('recipes/list', RecipeListView.as_view(), name="task_list"),
     path('recipe/1', recipe_one,name="recipe-one"),
     path('recipe/2', recipe_two,name="recipe-two"),
 ]
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.base import TemplateView
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 from .models import Recipe
 
 def recipe_list(request):
     recipes = Recipe.objects.all()
     ctx = {"recipes":recipes}
     return render(request, 'recipe_list.html',ctx)
+
+class RecipeListView(ListView):
+    model = Recipe
+    template_name = "recipe_list.html"
+
+class RecipeDetailView(DetailView):
+    model = Recipe
+    template_name = "recipe_list.html"
 
 # def recipe_list(request):
 #     ctx={
