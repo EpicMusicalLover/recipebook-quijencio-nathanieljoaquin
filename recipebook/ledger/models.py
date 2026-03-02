b71dc2e3e31b330714f91b66d80b3733759ac438
Nathaniel Joaquin Quijencio <natesjoaquin@gmail.com>
2026-03-02 08:54:16 +0800

    added profile model base in models.py

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
 
@@ -33,3 +34,6 @@ class RecipeIngredient(models.Model):
 
     def __str__(self):
         return f"{self.quantity} {self.ingredient.name}"
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
