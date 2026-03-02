9ace270db6e6fcde6fe9cfea85435210a05504cb
Nathaniel Joaquin Quijencio <natesjoaquin@gmail.com>
2026-03-02 20:28:39 +0800

    removed the base template extension and headers in the password reset templates due to it not working as intended

--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -133,3 +133,4 @@ USE_TZ = True
 STATIC_URL = "static/"
 LOGIN_REDIRECT_URL = "ledger:recipe_list"
 LOGOUT_REDIRECT_URL = "/accounts/login/"
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
