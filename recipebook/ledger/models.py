357360eeea9e5c04b374c637f04ee14ad913a701
Nathaniel Joaquin Quijencio <natesjoaquin@gmail.com>
2026-03-02 17:27:41 +0800

    reformatted some files according to pep8 coding style

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -2,12 +2,15 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
-    short_bio = models.TextField(blank = True)
+    short_bio = models.TextField(blank=True)
+
     def __str__(self):
         return self.name
+
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
@@ -26,9 +29,10 @@ class Recipe(models.Model):
     )
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.name
-    
+
     def get_absolute_url(self):
         return reverse("ledger:recipe_detail", args=[self.pk])
 
@@ -44,4 +48,3 @@ class RecipeIngredient(models.Model):
 
     def __str__(self):
         return f"{self.quantity} {self.ingredient.name}"
-    
