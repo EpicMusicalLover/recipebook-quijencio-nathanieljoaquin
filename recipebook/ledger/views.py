5614e6b3fe9cefd1d427f74ca22c96f1be5ca474
Nathaniel Joaquin Quijencio <natesjoaquin@gmail.com>
2026-03-02 14:14:33 +0800

    added LoginRequiredMixin module in views.py for the recipe detail view to restrict view of that page for logged in users only

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from .models import Recipe
@@ -8,7 +9,7 @@ class RecipeListView(ListView):
     template_name = "recipe_list.html"
 
 
-class RecipeDetailView(DetailView):
+class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = "recipe_details.html"
 
