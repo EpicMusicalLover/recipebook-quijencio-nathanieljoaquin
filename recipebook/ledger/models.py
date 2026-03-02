a9b87b3c55315a400cf53a5e300b74ef61e525ff
Nathaniel Joaquin Quijencio <natesjoaquin@gmail.com>
2026-03-02 09:12:42 +0800

    rearranged the order of models in models.py and also added the author foreign key and created_on and updated_on fields inside of the recipe model

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -2,6 +2,12 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    short_bio = models.TextField(blank = True)
+    def __str__(self):
+        return self.name
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
@@ -15,10 +21,14 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
-
+    author = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, related_name="recipes"
+    )
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
     def __str__(self):
         return self.name
-
+    
     def get_absolute_url(self):
         return reverse("ledger:recipe_detail", args=[self.pk])
 
@@ -35,9 +45,3 @@ class RecipeIngredient(models.Model):
     def __str__(self):
         return f"{self.quantity} {self.ingredient.name}"
     
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=50)
-    short_bio = models.TextField(blank = True)
-    def __str__(self):
-        return self.name
