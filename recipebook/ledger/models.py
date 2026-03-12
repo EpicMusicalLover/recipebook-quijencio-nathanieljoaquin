ee7a68ac1b8deb3d0b8ec7cd15fe1206e9b8349a
Nathaniel Joaquin Quijencio <natesjoaquin@gmail.com>
2026-03-12 08:45:00 +0800

    added the recipeimage model in models.py

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -2,7 +2,6 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
 
-
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
@@ -36,6 +35,12 @@ class Recipe(models.Model):
     def get_absolute_url(self):
         return reverse("ledger:recipe_detail", args=[self.pk])
 
+class RecipeImage(models.Model):
+    image = models.ImageField(blank = False, null = False)
+    description = models.CharField(max_length= 255)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='Images')
+    def __str__(self):
+        return self.description
 
 class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=100)
