01fec3acaeef0021949f947411f24d7b1e5dc454
Nathaniel Joaquin Quijencio <natesjoaquin@gmail.com>
2026-03-12 14:10:10 +0800

    added the media root and url in settings.py

--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -134,3 +134,5 @@ STATIC_URL = "static/"
 LOGIN_REDIRECT_URL = "ledger:recipe_list"
 LOGOUT_REDIRECT_URL = "/accounts/login/"
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
