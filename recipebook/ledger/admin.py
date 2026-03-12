58c0d581dcb7019a31180e8252a77e3895586d9c
Nathaniel Joaquin Quijencio <natesjoaquin@gmail.com>
2026-03-12 08:52:42 +0800

    added the recipeimageinline admin in admin.py and added the image function for recipe detail template

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
-from .models import Recipe, RecipeIngredient
+from .models import Recipe, RecipeIngredient, RecipeImage
 
 
 class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
 
+class RecipeImageInline(admin.TabularInline):
+    model = RecipeImage
 
 class RecipeAdmin(admin.ModelAdmin):
-    inlines = [RecipeIngredientInline]
+    inlines = [RecipeIngredientInline, RecipeImageInline]
 
 
 admin.site.register(Recipe, RecipeAdmin)
