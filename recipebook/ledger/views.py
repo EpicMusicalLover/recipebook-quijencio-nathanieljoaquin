2a4c0cdb3c9330bd6202e91056a2b4e093680ac9
Nathaniel Joaquin Quijencio <natesjoaquin@gmail.com>
2026-03-12 13:21:58 +0800

    created new template for adding images, and creating the RecipeImageCreateView for views.py

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView
+from django.urls import reverse_lazy
 from .models import Recipe, RecipeImage
 from .forms import RecipeForm, RecipeImageForm
 
@@ -23,7 +24,17 @@ class RecipeCreateView(LoginRequiredMixin, CreateView):
 class RecipeImageCreateView(LoginRequiredMixin, CreateView):
     model = RecipeImage
     form_class = RecipeImageForm
-    template_name = "recipe_image_add.html"
+    template_name = "recipe_add_image.html"
+    def get_context_data(self, **kwargs): #to show the dropdown list
+        context = super().get_context_data(**kwargs)
+        context['taskgroup']=TaskGroup.objects.all()
+        context['form']=TaskForm()
+        return context
+    def post(self, request, *args, **kwargs): #whenever you type, itll post on the website
+        form = TaskForm(request.POST)
+        if form.is_valid():
+    def get_success_url(self):
+        return reverse_lazy('url name', kwargs={ 'pk': self.object.pk })
 
 
 # Create your views here.
