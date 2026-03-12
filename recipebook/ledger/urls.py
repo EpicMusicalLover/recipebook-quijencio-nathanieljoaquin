638c8c44c0b10d538ac81c10ae4c6e14adf0fd52
Nathaniel Joaquin Quijencio <natesjoaquin@gmail.com>
2026-03-12 13:09:28 +0800

    created the form for uploading new images, created the corresponding view in views.py, and then added a new path for adding images in urls.py

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import RecipeListView, RecipeDetailView, RecipeCreateView
+from .views import RecipeListView, RecipeDetailView, RecipeCreateView, RecipeImageCreateView
 
 app_name = "ledger"
 urlpatterns = [
     path("recipes/list/", RecipeListView.as_view(), name="recipe_list"),
     path("recipe/<int:pk>/", RecipeDetailView.as_view(), name="recipe_detail"),
     path("recipe/add", RecipeCreateView.as_view(), name="recipe_add"),
+    path('recipe/<int:pk>/add_image', RecipeImageCreateView.as_view(), name='recipe_image_add'),
 ]
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -2,8 +2,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView
-from .models import Recipe
-from .forms import RecipeForm
+from .models import Recipe, RecipeImage
+from .forms import RecipeForm, RecipeImageForm
 
 
 class RecipeListView(ListView):
@@ -19,6 +19,11 @@ class RecipeCreateView(LoginRequiredMixin, CreateView):
     model = Recipe
     template_name = "recipe_add.html"
     form_class = RecipeForm
+
+class RecipeImageCreateView(LoginRequiredMixin, CreateView):
+    model = RecipeImage
+    form_class = RecipeImageForm
+    template_name = "recipe_image_add.html"
 
 
 # Create your views here.
